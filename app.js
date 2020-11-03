61a116646444b00f50c974a1c3cc293239e95314
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-11-03 12:35:45 +0100

    binded sing function to sing button

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-const friends = ['Nello', 'Maio', 'Pit', 'Pelle', 'Paolo'];
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('dom loaded');
+  // create button
+  const singButtun = document.createElement('button');
+  singButtun.innerText = 'Sing!';
+  const container = document.querySelector('.container');
+  container.insertBefore(singButtun, document.querySelector('.friend'));
+  // ---------------------
 
-for (let i = 0; i < friends.length; i++) {
-  let chorus = '';
-  for (let j = 99; j >= 1; j--) {
-    if (j === 1) {
-      chorus += `${j} line of code in the file, ${j} line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file
-      `;
-    } else {
-      chorus += `${j} lines of code in the file, ${j} lines of code; ${
-        friends[i]
-      } strikes one out, clears it all out, ${
-        j - 1
-      } lines of code in the file\n`;
+  const friends = ['Nello', 'Maio', 'Pit', 'Pelle', 'Paolo'];
+
+  const sing = () => {
+    for (const friend of friends) {
+      let chorus = '';
+      for (let j = 99; j >= 1; j--) {
+        switch (j) {
+          case 1:
+            chorus += `${j} line of code in the file, ${j} line of code; ${friend} strikes one out, clears it all out, no more lines of code in the file`; //last line
+            break;
+          case 2:
+            chorus += `${j} lines of code in the file, ${j} lines of code; ${friend} strikes strikes one out, 1 line of code in the file\n`; //2nd top last grammer issue fixed
+            break;
+          default:
+            chorus += `${j} lines of code in the file, ${j} lines of code; ${friend} strikes one out, ${
+              j - 1
+            } lines of code in the file\n`; //all other lines
+            break;
+        }
+      }
+      console.log(`${friend}:\n${chorus}`);
     }
-  }
+  };
 
-  // console.log(friends[i] + ':\n', chorus);
-  console.log(`${friends[i]}:\n${chorus}`);
-}
+  singButtun.addEventListener('click', sing);
+  // remove div friend
+  document.querySelector('.friend').remove();
+});
